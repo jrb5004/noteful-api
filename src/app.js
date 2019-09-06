12ef5d17db9ace9ad719706cfca49f3520e32c90
Joe Baragona <jrb5004@gmail.com>
2019-09-06 14:08:08 -0700

    updated file name

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const foldersRouter = require('./folders/folders-router')
+const foldersRouter = require('./Folders/folders-router')
 const notesRouter = require('./notes/notes-router')
 
 const app = express()
